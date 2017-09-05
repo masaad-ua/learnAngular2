2ed2c4b8c7f6475bc75549c537b2fb448ba65719
Maksym Osypchuk <max-modern@mail.ru>
2017-09-05 21:31:00 +0300

    update (index.html):update tamplate

--- built/hello-angular.js
+++ built/hello-angular.js
@@ -1,4 +1,5 @@
-System.register(['@angular/core'], function(exports_1) {
+System.register(["@angular/core"], function (exports_1, context_1) {
+    "use strict";
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,15 +9,16 @@ System.register(['@angular/core'], function(exports_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
-    var HelloAngularComponent;
+    var __moduleName = context_1 && context_1.id;
+    var core_1, HelloAngularComponent;
     return {
-        setters:[
+        setters: [
             function (core_1_1) {
                 core_1 = core_1_1;
-            }],
-        execute: function() {
-            HelloAngularComponent = (function () {
+            }
+        ],
+        execute: function () {
+            HelloAngularComponent = /** @class */ (function () {
                 function HelloAngularComponent() {
                     this.greeting = 'Hello Angular 2!';
                 }
@@ -24,11 +26,11 @@ System.register(['@angular/core'], function(exports_1) {
                     core_1.Component({
                         selector: 'hello-angular',
                         template: '<h1>{{greeting}}</h1>'
-                    }), 
-                    __metadata('design:paramtypes', [])
+                    }),
+                    __metadata("design:paramtypes", [])
                 ], HelloAngularComponent);
                 return HelloAngularComponent;
-            })();
+            }());
         }
-    }
+    };
 });
--- systemjs.config.js
+++ systemjs.config.js
@@ -1,27 +1,31 @@
-(function (global) {
-  System.config({
-    map: {
-      app: 'app',
-        
-      '@angular/core': 'node_modules/@angular/core/bundles/core.umd.js',
-      '@angular/common': 'node_modules/@angular/common/bundles/common.umd.js',
-      '@angular/compiler': 'node_modules/@angular/compiler/bundles/compiler.umd.js',
-      '@angular/platform-browser': 'node_modules/@angular/platform-browser/bundles/platform-browser.umd.js',
-      '@angular/platform-browser-dynamic': 'node_modules/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-      '@angular/http': 'node_modules/@angular/http/bundles/http.umd.js',
-      '@angular/router': 'node_modules/@angular/router/bundles/router.umd.js',
-      '@angular/forms': 'node_modules/@angular/forms/bundles/forms.umd.js',
+(function(){
+  var pathMappings = {
+    '@angular': 'node_modules/@angular',
+    'rxjs': 'node_modules/rxjs'
+  };
+  var packages = [
+    '@angular/common',
+    '@angular/compiler',
+    '@angular/core',
+    '@angular/http',
+    '@angular/platform-browser',
+    '@angular/platform-browser-dynamic',
+    '@angular/router',
+    '@angular/router-deprecated',
+    '@angular/testing',
+    'rxjs',
+    'built'
+  ];
+  var packagesConfig = {};
 
-      'rxjs': 'node_modules/rxjs'
-    },
-    packages: {
-      app: {
-        main: './main.js',
-        defaultExtension: 'js'
-      },
-      rxjs: {
-        defaultExtension: 'js'
-      }
-    }
+  packages.forEach(function(packageName) {
+    packagesConfig [packageName] = {
+      main: 'index.js',
+      defaultExtension: 'js'
+    };
   });
-})(this);
+  System.config({
+    map: pathMappings,
+    packages: packagesConfig
+  });
+})();
