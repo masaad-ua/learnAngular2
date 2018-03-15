9f3580b3cbfd43c0c773452fbd6f236dd71a2c80
Maksym Osypchuk <maksym3d@gmail.com>
2018-03-15 20:06:13 +0200

    update(zTR41pomodoroTask): update app

--- TR41pomodoroTask/systemjs.config.js
+++ TR41pomodoroTask/systemjs.config.js
@@ -1,7 +1,7 @@
 (function (global) {
   System.config({
     map: {
-      app: 'built',
+      app: 'app',
         
       '@angular/core': 'node_modules/@angular/core/bundles/core.umd.js',
       '@angular/common': 'node_modules/@angular/common/bundles/common.umd.js',
